9cf16eec58478f59266a34619a9dcb2dde2cbed3
Daneswara <150428419+codewara@users.noreply.github.com>
2025-06-18 17:44:23 +0700

    add mongo connctor in lib

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,11 @@
+// src/lib/mongodb.ts
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGODB_URI!;
+const options = { };
+
+if (!process.env.MONGODB_URI) throw new Error('Please add your Mongo URI to .env.local');
+const client: MongoClient = new MongoClient(uri, options);
+const clientPromise: Promise<MongoClient> = client.connect();
+
+export default clientPromise;
